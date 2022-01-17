38e114ffeeac9fa3ab0ef5efcec0c9e986b0e560
Mira Black <justmirablack@gmail.com>
2022-01-17 16:16:56 +0300

    add files

---
+++ init.sh
@@ -0,0 +1,4 @@
+sudo mv /home/box/StepikNginx/web ~
+sudo ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
